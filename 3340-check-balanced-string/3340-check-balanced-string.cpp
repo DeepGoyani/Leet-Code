2ae3314e590acc64dd4f5d01202d2208370f74ad
Deep Goyani <177735104+DeepGoyani@users.noreply.github.com>
2026-02-15 22:25:00 +0530

    Time: 0 ms (100%), Space: 8.5 MB (66.99%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int odd=0;
+        int even=0;
+        for(int i=0;i<num.length();i++){
+            if(i%2==0)odd+=(num[i]-'0');
+            else even+=(num[i]-'0');
+        }
+        return odd==even;
+    }
+};
