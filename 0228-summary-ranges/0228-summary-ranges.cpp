8de1a3405269e27fca6e8d8b269eaa9e9c03dbc8
Deep Goyani <177735104+DeepGoyani@users.noreply.github.com>
2026-03-02 23:59:30 +0530

    Time: 0 ms (100%), Space: 9.1 MB (94.92%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -13,19 +13,16 @@ public:
         for (int i = 0; i < nums.size();) {
             int start = nums[i];
             int j = i;
-            // Expand the range as long as elements are consecutive
             while (j + 1 < nums.size() && nums[j + 1] == nums[j] + 1) {
                 j++;
             }
 
-            // Format the range string
             if (nums[j] == start) {
                 result.push_back(std::to_string(start));
             } else {
                 result.push_back(std::to_string(start) + "->" + std::to_string(nums[j]));
             }
             
-            // Move to the next potential start of a range
             i = j + 1;
         }
         return result;
