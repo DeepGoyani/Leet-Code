6f4f81403d5ed3f14c3316b231ec04a86e8c2e12
Deep Goyani <177735104+DeepGoyani@users.noreply.github.com>
2026-03-03 13:34:02 +0530

    Time: 1 ms (73.71%), Space: 13.7 MB (47.19%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> st;
+        int len=s.size();
+        if(len==0) return 0;
+        long long operand=0,res=0,sign=1; 
+        for(int i=0;i<len;i++) {
+            if(isdigit(s[i])) {
+                operand=operand*10+(s[i]-'0');
+            } else if(s[i]=='+') {
+                res+=sign*operand;
+                sign=1;
+                operand=0;
+            } else if(s[i]=='-') {
+                res+=sign*operand;
+                sign=-1;
+                operand=0;
+            } else if(s[i]=='(') {
+                st.push(res);
+                st.push(sign);
+                sign=1;
+                res=0;
+            } else if(s[i]==')') {
+                res+=sign*operand;
+                res*=st.top();
+                st.pop(); 
+                res+=st.top();
+                st.pop();
+                operand=0;
+            }
+        }
+        return res+(sign*operand);
+    }
+};
